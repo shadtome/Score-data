4ad6f167f0fd217a8d55d318a145305f1cc734d2
shadtome <catipton7777@gmail.com>
2024-10-17 17:16:09 -0700

    Made the match_id extraction save the match_ids

--- get_data/Scrapers/sofascore_scraper.py
+++ get_data/Scrapers/sofascore_scraper.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from tqdm.auto import tqdm
+import tqdm as tqdm_m
 import http.client, json
 from urllib.parse import urlparse
 import requests
@@ -19,16 +20,15 @@ import random
 
 
 class sofascore:
-    def __init__(self,country,league,league_id,season_id):
+    def __init__(self,country,league,league_id,season_id,num_rounds):
         self.country = country
         self.league = league
         self.season_id = season_id
         self.league_id = league_id
+        self.num_rounds = num_rounds
         self.driver = self.initialize_driver()
         self.match_ids,round_clicks = self.scrape_matches()
         
-
-    # Initialize the WebDriver with custom options
     def initialize_driver(self):
         chrome_options = ChromeOptions()
         # Set the page load strategy to 'none'
@@ -36,7 +36,6 @@ class sofascore:
         
         return webdriver.Chrome(options=chrome_options)
     
-
     def navigate_to_page(self,url):
         """Navigate to the given URL without waiting for the full page load."""
         
@@ -68,14 +67,17 @@ class sofascore:
             time.sleep(1)  # Allow time for full rendering after scroll
 
             match_links = self.driver.find_elements(By.CSS_SELECTOR, 'a[href*="/football/match/"]')
+            
             #print(f"Round {round_number}: Match links located (count: {len(match_links)}).")
 
             if len(match_links) < 10:
                 print(f"Warning: Less than 10 matches found for round {round_number}, possible loading issue.")
-                self.refresh_page()
+                
 
             for match in match_links:
+                
                 match_url = match.get_attribute('href')
+                
                 match_id = match_url.split('#id:')[-1]
                 
                 if match_id not in match_ids:
@@ -103,7 +105,8 @@ class sofascore:
         try:
             
             left_arrow = WebDriverWait(self.driver, 10).until(
-                EC.visibility_of_element_located((By.CSS_SELECTOR, "button.Button.iCnTrv[style*='visibility: visible;']"))
+                #EC.visibility_of_element_located((By.CSS_SELECTOR, "button.Button.iCnTrv[style*='visibility: visible;']"))
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "button.Button.iCnTrv[style*='visibility: visible;']"))
             )
             
             if left_arrow.is_displayed() and left_arrow.is_enabled():
@@ -117,7 +120,7 @@ class sofascore:
     def scrape_matches(self):
         """Main function to scrape match IDs from the Premier League page."""
         match_ids = []
-        total_rounds = 38  # Number of rounds in the Premier League
+        total_rounds = self.num_rounds  # Number of rounds in the league
         round_click_count = 0
 
         # Navigate to the Premier League page for the desired season
@@ -152,17 +155,129 @@ class sofascore:
         self.driver.close()
         return match_ids, round_click_count
     
+class get_all_ids:
+    def __init__(self):
+        None
+        
+    def collect_match_ids(self):
+        match_ids = dict()
+        id_dict = self.league_season_ids_from_file()
+
+        #id_dict = {"england": {"premier-league": {"league_id": 17,"seasons": {"23/24": {"id":52186,"rounds":38,"matches":380, 'name': 'Premier League'}}}},
+                   #"spain" : {"laliga": {"league_id": 8, "seasons": {"23/24": {"id":52376,"rounds":38,"matches":380,"name":"LaLiga"}}}}}
+        
+        for country,country_v in tqdm(id_dict.items(),leave=False,desc='country',position=0):
+            for league, league_v  in tqdm(country_v.items(),leave=False,desc='leagues',position=1):
+                for season_year,season_v in tqdm(league_v['seasons'].items(),leave=False,desc='season_ids',position=2):
+                            
+                            
+                            xm = season_v['matches']
+                            list_ids = sofascore(country,league,league_v['league_id'],season_v['id'],xm).match_ids
+                            
+                            match_ids = match_ids | {country : 
+                                                            {league: 
+                                                                    {season_year: {
+                                                                                    "match_ids": list_ids,
+                                                                                    "matches": xm,
+                                                                                    "name": season_v['name']} }} }
+        return match_ids
     
+    def list_match_ids(self):
+        map_match_ids = self.match_ids_from_file()
+        match_ids_list = []
+        for country, country_v in map_match_ids.items():
+            for league,league_v in country_v.items():
+                for season_year,season_v in league_v.items():
+                    match_ids_list += season_v['match_ids']
+        return match_ids_list
+    
+    def clean_ids(self):
+        match_ids = self.match_ids_from_file()
+        for country, country_v in match_ids.items():
+            for league,league_v in country_v.items():
+                for season_year,season_v in league_v.items():
+                    xm = season_v['matches']
+                    list_data = season_v['match_ids']
+                    name = season_v['name']
+                    list_data = self.clean_season_ids(name,season_year,xm,list_data)
+                    season_v['match_ids'] = list_data
+        path = os.getcwd()
+        path = os.path.join(path,'pre_data/sofascore/match_ids.json')      
+        with open(path, 'w',encoding='utf-8') as json_file:
+            json.dump(match_ids,json_file, ensure_ascii=False,indent=4)
+    
+    def clean_season_ids(self,xleague,xseason,xm,list_data):
+        """Go through the current match ids to make sure they are in the correct season and league."""
+        if len(list_data) == xm:
+            return list_data
+        removed_list = []
+        league_list = []
+        i=0
+        pbar = tqdm(total=len(list_data),ascii="--")
+        while i<len(list_data):
+            
+            time.sleep(random.randint(0,2))
+            
+            url_date = "https://www.sofascore.com/api/v1/event/"+list_data[i]
+            parsed_url_date = urlparse(url_date)
+            conn_date = http.client.HTTPSConnection(parsed_url_date.netloc)
+            conn_date.request("GET",parsed_url_date.path)
+            res_date = conn_date.getresponse()
+            date_json = res_date.read()
+            json_data_date = json.loads(date_json.decode("utf-8"))
+            league = json_data_date['event']['tournament']['name']
+            season = json_data_date['event']['season']['year']
+            conn_date.close()
+            
+            # lets compare this with what the actual league and season should be.
+            if league != xleague or season != xseason:
+                removed_list.append(list_data.pop(i))
+                league_list.append(league)
+                pbar.update(2)
+            else:
+                i+=1
+                pbar.update(1)
+
+        pbar.close()
+        print(f'removed the elements {removed_list}, leaving {len(list_data)} left over\n {league_list}')
+        return list_data
+            
+
+
+    def collect_and_save(self):
+        match_ids_map = self.collect_match_ids()
+        path = os.getcwd()
+        path = os.path.join(path,'pre_data/sofascore/match_ids.json')
+        with open(path, 'w',encoding='utf-8') as json_file:
+            json.dump(match_ids_map,json_file, ensure_ascii=False,indent=4)
+
+    def match_ids_from_file(self):
+        path = os.getcwd()
+        path = os.path.join(path, 'pre_data/sofascore/match_ids.json')
+        with open(path,'r') as json_file:
+            match_ids = json.load(json_file)
+        return match_ids
+
+    def league_season_ids_from_file(self):
+        path = os.getcwd()
+        path = os.path.join(path,'pre_data')
+        if os.path.exists(path)==False:
+            os.mkdir(path)
+        path = os.path.join(path,'sofascore')
+        if os.path.exists(path)==False:
+            os.mkdir(path)
+        path = os.path.join(path,'league_seasons.json')
+        with open(path,'r') as json_file:
+            data_dict = json.load(json_file)
+        return data_dict
+
         
 
-class get_all:
+class get_all_data:
     def __init__(self):
-        self.id_dict = self.get_league_season_ids()
-        #self.id_dict = {"england": {"premier-league": {"league_id": 17,"seasons": {"23/24": 52186}}}}
-        self.match_ids = self.get_match_ids()
+        self.match_ids = get_all_ids().list_match_ids()
         self.data = self.get_data()
         self.save_data()
-
 
     def get_data(self):
         top_columns = ['name','shortName','position','height','dateOfBirthTimestamp']
@@ -192,7 +307,7 @@ class get_all:
         num_players = 0
         for id_x in tqdm(self.match_ids,leave=False,desc='main loop',position=0):
             time.sleep(random.randint(0,2))
-            #lets get match data
+            
             
             url_date = "https://www.sofascore.com/api/v1/event/"+id_x
             parsed_url_date = urlparse(url_date)
@@ -257,29 +372,7 @@ class get_all:
         data_df = pd.DataFrame(data)
         data_df['dateOfBirthTimestamp'] = pd.to_datetime(data_df['dateOfBirthTimestamp'])
         return data_df
-    
-    def get_match_ids(self):
-        match_ids = []
-        for country,country_v in tqdm(self.id_dict.items(),leave=False,desc='country',position=0):
-            for league, league_v  in tqdm(country_v.items(),leave=False,desc='leagues',position=1):
-                for season_year,season_id in tqdm(league_v['seasons'].items(),leave=False,desc='season_ids',position=2):
-                            match_ids += sofascore(country,league,league_v['league_id'],season_id).match_ids
-        return match_ids
-    
-    def get_league_season_ids(self):
-        path = os.getcwd()
-        path = os.path.join(path,'pre_data')
-        if os.path.exists(path)==False:
-            os.mkdir(path)
-        path = os.path.join(path,'sofascore')
-        if os.path.exists(path)==False:
-            os.mkdir(path)
-        path = os.path.join(path,'league_seasons.json')
-        with open(path,'r') as json_file:
-            data_dict = json.load(json_file)
-        return data_dict
-
-        
+       
     def save_data(self):
         path = os.getcwd()
         path = os.path.join(path,'data/sofascore')
