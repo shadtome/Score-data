a19b89f04cc31bd11e049990ca251b6fa5815817
shadtome <catipton7777@gmail.com>
2024-10-23 09:18:55 -0700

    Made init_get_main_data make a database file

--- init_get_main_data.py
+++ init_get_main_data.py
@@ -1,5 +1,8 @@
 import get_data.Scrapers.get_kaggle as gk
 import get_data.merge_data.merge_transfer_sofascore as mts
+import sqlite3
+import pandas as pd
+import os
 
 # get all kaggle data
 print('Start kaggle data download')
@@ -7,3 +10,15 @@ gk.get_transfermarkt_data()
 gk.get_sofascore_data()
 print('Done with kaggle data\n Start Merge')
 mts.merge()
+
+sql_file = 'data/main_data/main_data.db'
+con = sqlite3.connect(sql_file)
+
+    
+df = pd.read_csv('data/main_data/main_data_sofascore.csv')
+
+table_name = 'player_stats'
+df.to_sql(table_name,con,if_exists='replace',index=False)
+
+con.commit()
+con.close()
